0a6a123567d0bf9c607e198e1df11ffbb146b89f
Craig Peterson <craig.peterson0701@gmail.com>
2024-10-02 15:18:56 -0500

    back-end stuff

---
+++ Backend/src/test/java/CS_3365/MSB/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CS_3365.MSB.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
