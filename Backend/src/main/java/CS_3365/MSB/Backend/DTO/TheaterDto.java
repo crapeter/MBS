b8893fcbd23b2de5aa1f0c583e09cf3300f2ec4a
Craig Peterson <craig.peterson0701@gmail.com>
2024-10-09 15:12:18 -0500

    Json Properties for Theater Dto

--- Backend/src/main/java/CS_3365/MSB/Backend/DTO/TheaterDto.java
+++ Backend/src/main/java/CS_3365/MSB/Backend/DTO/TheaterDto.java
@@ -1,10 +1,16 @@
 package CS_3365.MSB.Backend.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TheaterDto {
+  @JsonProperty("location")
   private String location;
+
+  @JsonProperty("roomNumber")
   private int roomNumber;
+
+  @JsonProperty("seatsBooked")
   private int seatsBooked;
 }
