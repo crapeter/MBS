ad6029f41f1e69e770d376342e7fa5b44f20a623
Craig Peterson <craig.peterson0701@gmail.com>
2024-10-30 13:10:44 -0500

    password confirmation

--- Frontend/src/Components/Login/NewUser.js
+++ Frontend/src/Components/Login/NewUser.js
@@ -9,6 +9,7 @@ const NewUser = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [address, setAddress] = useState('')
   const [cardNumber, setCardNumber] = useState('')
@@ -16,6 +17,10 @@ const NewUser = () => {
   const nav = useNavigate()
 
   const handleRegistation = async () => {
+    if (password !== confirmPassword) {
+      alert("Passwords do not match")
+      return
+    }
     await axios.post(`/api/users/new/user`, {
       isAdmin: isAdmin,
       name: name,
@@ -71,6 +76,16 @@ const NewUser = () => {
               type="password"
               placeholder="Password"
               onChange={(e) => setPassword(e.target.value)}
+              requried
+            />
+          </Form.Group>
+          
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control
+              className="register_input"
+              type="password"
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
               requried
             />
           </Form.Group>
